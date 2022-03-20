2f6d4afd50d228daba8e3bbd3c4eac5f59826730
ChocRobot <33766119@student.murdoch.edu.au>
2022-03-20 17:37:15 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer class="container-fluid text-center">
+    <div class="container">
+<div class="row">
+<div class="col-md-6">
+  <a class="navbar-brand" href="#"> <span class="josh"> JOSH </span>  <span class="orange"> WHITKIN </span></a>
+
+
+</div>
+
+<div class="col-md-6">
+    <p>terms and conditions</p>
+    
+    
+    </div>
+
+
+</div>
+    </div>
+
+   </footer> 
+   <?php wp_footer(); ?>
+</body>
+</html>
