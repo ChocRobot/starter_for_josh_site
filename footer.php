02867b71f6eed4f24d82663d7fada8e43a02f597
ChocRobot <33766119@student.murdoch.edu.au>
2022-04-24 16:06:24 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,14 +2,15 @@
     <div class="container">
 <div class="row">
 <div class="col-md-6">
-  <a class="navbar-brand" href="#"> <span class="josh"> JOSH </span>  <span class="orange"> WHITKIN </span></a>
-
-
-</div>
+  <!-- <a class="navbar-brand" href="#"> <span class="josh"> JOSH </span>  <span class="orange"> WHITKIN </span></a> -->
+  <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
+   
 
 <div class="col-md-6">
-    <p>terms and conditions</p>
-    
+    <!-- <p>terms and conditions</p> -->
+    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
+endif; ?>
     
     </div>
 
